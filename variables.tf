9024bef83c799ae86b679028fff7e2f4aada9bc1
balunagalli <89579055+balunagalli@users.noreply.github.com>
2023-01-16 14:09:13 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "location"{
     description = "Localtion of rg"
     default = "EastUS"
 }
+
+variable "tfc_org_name"{
+    description = "terraform organization name"
+    default = "bala-tf"
+}
+
+variable "tfc_host_name"{
+    description = "terraform host name"
+    default = "app.terraform.io"
+}
