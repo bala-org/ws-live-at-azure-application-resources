375daf1ab1d804e1434a8808f9ae0b5f3150dc2c
balunagalli <89579055+balunagalli@users.noreply.github.com>
2023-01-16 15:07:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,13 +16,13 @@ resource "azurerm_resource_group" "test-rg" {
     
 }
 
-# data "terraform_remote_state" "baseline" {
-#   backend = "remote"
-#   config = {
-#     organization = var.tfc_org_name
-#     hostname     = var.tfc_host_name
-#     workspaces = {
-#       name = var.tfc_baseline_repo
-#     }
-#   }
-# }
+ data "terraform_remote_state" "baseline" {
+   backend = "remote"
+   config = {
+     organization = var.tfc_org_name
+     hostname     = var.tfc_host_name
+     workspaces = {
+       name = var.tfc_baseline_repo
+     }
+   }
+ }
