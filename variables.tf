b2900d1a86ca159c8d58a238a5e3cab746c2a44f
balunagalli <89579055+balunagalli@users.noreply.github.com>
2023-01-19 18:58:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,19 +8,19 @@ variable "region_primary"{
     default = ""
 }
 
-variable "tfc_org_name"{
-    description = "terraform organization name"
-    default = ""
-}
+#variable "tfc_org_name"{
+#    description = "terraform organization name"
+#    default = ""
+#}
 
-variable "tfc_host_name"{
-    description = "terraform host name"
-    default = ""
-}
+#variable "tfc_host_name"{
+#    description = "terraform host name"
+#    default = ""
+#}
 
-variable "tfc_baseline_repo"{
-    description = "baseline repo to access outputs" 
-}
+#variable "tfc_baseline_repo"{
+#    description = "baseline repo to access outputs" 
+#}
 
 variable "vnetname" {
 description = "storage account name"
